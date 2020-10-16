94fe3103374bfec7986afb18c1bc7ca42bea42cc
Reginalda Santos <reginalda012@gmail.com>
2020-10-16 13:23:26 -0300

    Pojeto Relogio/Cronometro

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Contador from './Contador'
+import LabelCronometro from './LabelCronometro'
+import Botao from './Botao'
+
+function App() {
+  return (
+     <div>
+      <LabelCronometro name="Cronometro"/>
+      <Contador/>
+     </div>
+     
+ 
+    
+  );
+}
+
+export default App;
---
+++ src/LabelCronometro.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './App.css'
+
+const LabelCrometro = (props) => (
+    <h1 className="my-title">{props.name}</h1>
+)
+export default LabelCrometro
